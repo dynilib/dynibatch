3d3dcecd8d499c8bfbfb5d8f4cdc4397b7f0cfc5
Julien Ricard <julien.ricard@gmail.com>
2016-06-02 18:31:17 +0200

    Update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,14 @@
 import os
 import pytest
+import random
 
 import numpy as np
 
 from libdyni.utils.exceptions import ParameterError
 from libdyni.utils import segment
 from libdyni.utils import segment_container
+from libdyni.utils import feature_container
+from libdyni.utils import datasplit_utils
 from libdyni.utils import utils
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
@@ -19,7 +22,7 @@ TEST_FIRST_SEGMENT_DURATION = 0.79
 
 class TestSegment:
 
-    def test_working_case(self):
+    def test_ok(self):
         try:
             start_time = 1
             end_time = 10
@@ -159,3 +162,128 @@ class TestSegmentContainer:
             segment_container._parse_segment_file_line(line, ";"))
         assert (np.isclose(start_time, 0.12) and np.isclose(end_time, 0.15) and
                 label == "bird_a")
+
+        
+class TestFeatureContainer:
+
+    def test_init(self):
+        try:
+            feature_container.FeatureContainer(
+                    "fake_audio_path",
+                    22050,
+                    256,
+                    128)
+        except:
+            pytest.fail("Unexpected Error")
+
+    def test_features_ok(self):
+        features = ["feat1", "feat2"]
+        configs = ["config1", "config2"]
+        fc =  feature_container.FeatureContainer(
+                    "fake_audio_path",
+                    22050,
+                    256,
+                    128)
+        fc.features["feat1"]["data"] = np.random.sample(10)
+        fc.features["feat1"]["config"] = "config1"
+        fc.features["feat2"]["data"] = np.random.sample(10)
+        fc.features["feat2"]["config"] = "config2"
+        assert all(fc.has_features(list(zip(features, configs))))
+
+    def test_features_wrong_features(self):
+        features = ["feat1", "feat3"]
+        configs = ["config1", "config2"]
+        fc =  feature_container.FeatureContainer(
+                    "fake_audio_path",
+                    22050,
+                    256,
+                    128)
+        fc.features["feat1"]["data"] = np.random.sample(10)
+        fc.features["feat1"]["config"] = "config1"
+        fc.features["feat2"]["data"] = np.random.sample(10)
+        fc.features["feat2"]["config"] = "config2"
+        assert not all(fc.has_features(list(zip(features, configs))))
+
+    def test_features_wrong_configs(self):
+        features = ["feat1", "feat2"]
+        configs = ["config1", "config3"]
+        fc =  feature_container.FeatureContainer(
+                    "fake_audio_path",
+                    22050,
+                    256,
+                    128)
+        fc.features["feat1"]["data"] = np.random.sample(10)
+        fc.features["feat1"]["config"] = "config1"
+        fc.features["feat2"]["data"] = np.random.sample(10)
+        fc.features["feat2"]["config"] = "config2"
+        assert not all(fc.has_features(list(zip(features, configs))))
+
+    def test_features_empty_features(self):
+        features = ["feat1", "feat2"]
+        configs = ["config1", "config2"]
+        fc =  feature_container.FeatureContainer(
+                    "fake_audio_path",
+                    22050,
+                    256,
+                    128)
+        fc.features["feat1"]["data"] = np.random.sample(10)
+        fc.features["feat1"]["config"] = "config1"
+        fc.features["feat2"]["config"] = "config2"
+        assert not all(fc.has_features(list(zip(features, configs))))
+
+    def test_time_to_frame_ind(self):
+        sample_rate = 22050
+        win_size = 256
+        hop_size = 128
+        fc =  feature_container.FeatureContainer(
+                    "fake_audio_path",
+                    sample_rate,
+                    win_size,
+                    hop_size)
+        assert fc.time_to_frame_ind(0.015) == 2
+
+
+class TestUtils:
+
+    def test_get_n_overlapping_chunks(self):
+        file_duration = 12.5
+        seg_duration = 0.4
+        seg_overlap = 0.3
+
+        start = 0
+        hop = seg_duration * (1 - seg_overlap)
+        n_chunks = 0
+        while start + seg_duration < file_duration:
+            start += hop
+            n_chunks += 1
+
+        assert utils.get_n_overlapping_chunks(
+                file_duration,
+                seg_duration,
+                seg_overlap) == n_chunks
+
+
+class TestDatasplit:
+
+    fileset = set("f{}".format(i) for i in range(1000))
+
+    def test_create_datasplit_ok(self):
+        try:
+            train_set = set(random.sample(self.fileset, 700))
+            validation_set = set(random.sample(self.fileset-train_set, 100))
+            test_set = set(random.sample(self.fileset-train_set-validation_set, 200))
+            datasplit_utils.create_datasplit(train_set, validation_set, test_set,
+                    name="fake_datasplit")
+        except Exception as e:
+            pytest.fail("Unexpected Error: {}".format(e))
+
+    def test_create_datasplit_count(self):
+        train_set = set(random.sample(self.fileset, 700))
+        validation_set = set(random.sample(self.fileset-train_set, 100))
+        test_set = set(random.sample(self.fileset-train_set-validation_set, 200))
+        ds = datasplit_utils.create_datasplit(train_set, validation_set, test_set,
+                name="fake_datasplit")
+        assert (len(ds["sets"]["train"]) == 700 and
+                len(ds["sets"]["validation"]) == 100 and
+                len(ds["sets"]["test"]) == 200)
+
