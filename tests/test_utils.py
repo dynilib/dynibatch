05b9f3cf7236b8293fdfe9c4b5600dc780afb10a
Vincent ROGER <roger.vins.11@gmail.com>
2016-06-02 14:14:41 +0200

    Apply some recomendations of pylint

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,29 +1,28 @@
-import pytest
 import os
+import pytest
 
 import numpy as np
 
-from libdyni.features import segment_feature_extractor
 from libdyni.utils.exceptions import ParameterError
 from libdyni.utils import segment
 from libdyni.utils import segment_container
 from libdyni.utils import utils
 
 
-test_audio_path_tuple = ("data", "ID0132.wav")
-test_seg_path_tuple = ("data", "ID0132.seg")
-test_duration = 15.45
-test_n_segments = 4
-test_first_segment_duration = 0.79
+TEST_AUDIO_PATH_TUPLE = ("data", "ID0132.wav")
+TEST_SEG_PATH_TUPLE = ("data", "ID0132.seg")
+TEST_DURATION = 15.45
+TEST_N_SEGMENTS = 4
+TEST_FIRST_SEGMENT_DURATION = 0.79
 
 
 class TestSegment:
-    
+
     def test_working_case(self):
         try:
             start_time = 1
             end_time = 10
-            s = segment.Segment(start_time, end_time)
+            segment.Segment(start_time, end_time)
         except ParameterError:
             pytest.fail("Unexpected ParameterError")
 
@@ -31,15 +30,15 @@ class TestSegment:
         with pytest.raises(ParameterError):
             start_time = -1
             end_time = 10
-            s = segment.Segment(start_time, end_time)
+            segment.Segment(start_time, end_time)
 
     def test_time_order(self):
         with pytest.raises(ParameterError):
             start_time = 3
             end_time = 1
-            s = segment.Segment(start_time, end_time)
-        
-    
+            segment.Segment(start_time, end_time)
+
+
 class TestSegmentContainer:
 
     def test_no_segments_n_segments(self):
@@ -72,7 +71,7 @@ class TestSegmentContainer:
     def test_n_active_segments(self):
         sc = segment_container.SegmentContainer("fake_audio_path")
         n_segments = 5
-        active_segment_ind = [2 ,3]
+        active_segment_ind = [2, 3]
         for i in range(n_segments):
             sc.segments.append(segment.Segment(i * 1, (i + 1) * 1))
             if i in active_segment_ind:
@@ -96,7 +95,7 @@ class TestSegmentContainer:
     def test_n_active_segments_w_labels(self):
         sc = segment_container.SegmentContainer("fake_audio_path")
         n_segments = 5
-        active_segment_ind = [2 ,3]
+        active_segment_ind = [2, 3]
         for i in range(n_segments):
             sc.segments.append(segment.Segment(i * 1, (i + 1) * 1))
             if i in active_segment_ind:
@@ -106,32 +105,32 @@ class TestSegmentContainer:
     def test_create_segment_containers_from_audio_file_tuple(self):
         with pytest.raises(TypeError):
             sc = segment_container.create_segment_containers_from_audio_file(
-                    os.path.join(*test_audio_path_tuple))
+                os.path.join(*TEST_AUDIO_PATH_TUPLE))
 
     def test_create_segment_containers_from_audio_file_n_segment(self):
         sc = segment_container.create_segment_containers_from_audio_file(
-                    test_audio_path_tuple)
+            TEST_AUDIO_PATH_TUPLE)
         assert sc.n_segments == 1
 
     def test_create_segment_containers_from_audio_file_segment_duration(self):
         sc = segment_container.create_segment_containers_from_audio_file(
-                    test_audio_path_tuple)
-        assert np.abs(sc.segments[0].duration - test_duration) < 1e-03
+            TEST_AUDIO_PATH_TUPLE)
+        assert np.abs(sc.segments[0].duration - TEST_DURATION) < 1e-03
 
     def test_create_segment_containers_from_seg_file_tuple(self):
         with pytest.raises(TypeError):
             sc = segment_container.create_segment_containers_from_seg_file(
-                    os.path.join(*test_seg_path_tuple))
+                os.path.join(*TEST_SEG_PATH_TUPLE))
 
     def test_create_segment_containers_from_seg_file_n_segment(self):
         sc = segment_container.create_segment_containers_from_seg_file(
-                    test_seg_path_tuple)
-        assert sc.n_segments == test_n_segments
+            TEST_SEG_PATH_TUPLE)
+        assert sc.n_segments == TEST_N_SEGMENTS
 
     def test_create_segment_containers_from_seg_file_segment_duration(self):
         sc = segment_container.create_segment_containers_from_seg_file(
-                    test_seg_path_tuple)
-        assert np.abs(sc.segments[0].duration - test_first_segment_duration) < 1e-03
+            TEST_SEG_PATH_TUPLE)
+        assert np.abs(sc.segments[0].duration - TEST_FIRST_SEGMENT_DURATION) < 1e-03
 
     def test_split_data_duration(self):
         file_duration = 12.5
@@ -140,23 +139,22 @@ class TestSegmentContainer:
         segments = segment_container.split_data(file_duration, seg_duration,
                 seg_overlap)
         assert np.all(np.isclose(np.asarray([s.duration for s in segments]),
-            seg_duration))
+                                 seg_duration))
 
     def test_split_data_n_segments(self):
         file_duration = 12.5
         seg_duration = 0.4
         seg_overlap = 0.3
         segments = segment_container.split_data(file_duration, seg_duration,
-                seg_overlap)
+            seg_overlap)
         assert len(segments) == utils.get_n_overlapping_chunks(
-                file_duration,
-                seg_duration,
-                seg_overlap)
+            file_duration,
+            seg_duration,
+            seg_overlap)
 
     def test_parse_segment_file_line(self):
         line = "0.12; 0.15 ;  bird_a  "
         start_time, end_time, label = (
-                segment_container._parse_segment_file_line(line, ";"))
+            segment_container._parse_segment_file_line(line, ";"))
         assert (np.isclose(start_time, 0.12) and np.isclose(end_time, 0.15) and
                 label == "bird_a")
-
