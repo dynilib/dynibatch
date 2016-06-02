818e2f9be9f6ff15b8744fe4b2fff8cd0e396179
Julien Ricard <julien.ricard@gmail.com>
2016-06-02 15:42:24 +0200

    Remove useless properties

--- libdyni/utils/feature_container.py
+++ libdyni/utils/feature_container.py
@@ -2,17 +2,19 @@ import os
 from collections import defaultdict
 import joblib
 
+from libdyni.utils.exceptions import ParameterError
+
 FC_EXTENSION = ".fc.jl"
 
 
 class FeatureContainer:
     def __init__(self, audio_path, sample_rate, win_size, hop_size):
 
-        self._audio_path = audio_path  # relative to some root data path
-        self._sample_rate = sample_rate
-        self._win_size = win_size
-        self._hop_size = hop_size
-        self._features = defaultdict(dict)
+        self.audio_path = audio_path  # relative to some root data path
+        self.sample_rate = sample_rate
+        self.win_size = win_size
+        self.hop_size = hop_size
+        self.features = defaultdict(dict)
 
     def has_features(self, features):
         """Check whether the feature container has a set of features with a
@@ -20,29 +22,9 @@ class FeatureContainer:
         Args:
             features: list of tuple (name, config)
         Returns a list of booleans"""
-        return [name in self._features and config ==
-                self._features.get(name).get("config",
+        return [name in self.features and config ==
+                self.features.get(name).get("config",
                                              dict()) for name, config in features]
-
-    @property
-    def audio_path(self):
-        return self._audio_path
-
-    @property
-    def sample_rate(self):
-        return self._sample_rate
-
-    @property
-    def win_size(self):
-        return self._win_size
-
-    @property
-    def hop_size(self):
-        return self._hop_size
-
-    @property
-    def features(self):
-        return self._features
 
     @staticmethod
     def load(path):
@@ -62,8 +44,8 @@ class FeatureContainer:
                     os.path.join(path,
                                  os.path.splitext(
                                      os.path.basename(
-                                         self._audio_path))[0] + FC_EXTENSION),
+                                         self.audio_path))[0] + FC_EXTENSION),
                     compress=compress)
 
     def time_to_frame_ind(self, time):
-        return int(time * self._sample_rate / self._hop_size)
+        return int(time * self.sample_rate / self.hop_size)
