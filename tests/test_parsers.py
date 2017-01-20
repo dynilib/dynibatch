8acceaf01ef8a9e5b82f3ca64154d93241f4764b
Julien Ricard <julien.ricard@gmail.com>
2017-01-20 11:09:17 +0100

    Add tests for CVSSegmentLabelParser

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,12 +1,14 @@
 import os
 import pytest
 
-from libdyni.parsers.label_parsers import CSVFileLabelParser
+from libdyni.parsers.label_parsers import CSVFileLabelParser, CSVSegmentLabelParser
+from libdyni.utils.segment import CommonLabels
 
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
 
 TEST_FILE2LABEL_PATH = os.path.join(DATA_PATH, "file2label.csv")
+TEST_LABEL_PATH = os.path.join(DATA_PATH, "labels.txt")
 
 
 class TestCSVFileLabelParser:
@@ -21,3 +23,30 @@ class TestCSVFileLabelParser:
         parser = CSVFileLabelParser(TEST_FILE2LABEL_PATH)
         classes = parser.get_labels()
         assert parser.get_label("ID0131") == classes.index("bird_b")
+    
+    def test_get_labels(self):
+        parser = CSVFileLabelParser(TEST_FILE2LABEL_PATH)
+        classes = parser.get_labels()
+        assert classes == ["bird_a", "bird_b", "bird_c", "bird_d"]
+
+
+class TestCSVSegmentLabelParser:
+
+    def test_init(self):
+        try:
+            CSVSegmentLabelParser(DATA_PATH, TEST_LABEL_PATH)
+        except Exception as e:
+            pytest.fail("Unexpected Error: {}".format(e))
+
+    
+    def test_get_labels(self):
+        parser = CSVSegmentLabelParser(DATA_PATH, TEST_LABEL_PATH)
+        classes = parser.get_labels()
+        assert classes == ["bird_b", "bird_c"]
+    
+    def test_get_label(self):
+        parser = CSVSegmentLabelParser(DATA_PATH, TEST_LABEL_PATH,
+                seg_file_separator="\t")
+        classes = parser.get_labels()
+        assert list(parser.get_segment_container("ID0132.wav").labels)[0] == CommonLabels.unknown
+        assert list(parser.get_segment_container("ID0133.wav").labels)[0] == classes.index("bird_c")
