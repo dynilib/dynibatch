6f56957cdd4688c8f491f3db7d9c805626d43c19
Julien Ricard <julien.ricard@gmail.com>
2017-01-23 17:08:11 +0100

    Add doc.

--- libdyni/parsers/label_parsers.py
+++ libdyni/parsers/label_parsers.py
@@ -15,16 +15,41 @@ class SegmentLabelParser:
 
 
 class CSVFileLabelParser(FileLabelParser):
-    """csv label files parser
-    Args: file2label files, written as
-            <file_id><separator><class>
-            <file_id><separator><class>
-            <file_id><separator><class>
-          separator (optional): see above
-          label_file: file containing the set of labels to be used
+    """File-based label file parser (1 audio file = 1 label).
+
+    This object parses a CSV file (aka "file2label" file) written in the following format:
+    
+            <file_path><separator><label>
+            <file_path><separator><label>
+            <file_path><separator><label>
+            ...
+
+    where <file_path> is the path, relative to some root path, of an audio file
+    (see test/data/file2label.csv for an example).
+
+    An optional label_file argument can be set to constrain the set of labels to
+    be used. This file is a simple list of label, i.e.:
+
+            <label>
+            <label>
+            <label>
+            ...
+
+    If this argument is set, all files with a label which is not in the list specified in
+    label_file will have their label set to segment.CommonLabels.unknown.
     """
 
     def __init__(self, *file2label_files, separator=",", label_file=None):
+        """Create a label list and a file2label dict to quickly get the label from an audio
+        filename.
+
+        Args:
+            file2label_files: one or several file2label file.
+            separator (optional): character used as a separator in the
+                file2label file
+            label_file (optional): file containing the list of labels to be
+            used.
+         """
 
         # get label set
         if label_file:
@@ -49,13 +74,46 @@ class CSVFileLabelParser(FileLabelParser):
                         self._file2label_dict[sline[0].strip()] = self._label_list.index(label) if label in self._label_list else segment.CommonLabels.unknown
 
     def get_label(self, audio_path):
+        """Returns the label of audio_path
+
+        Args:
+            audio_path: (relative) audio path
+        """
         return self._file2label_dict[splitext(basename(audio_path))[0]]
 
     def get_labels(self):
+        """Returns the list of labels"""
         return self._label_list
 
 
 class CSVSegmentLabelParser(SegmentLabelParser):
+    """Segment-based label file parser (1 segment = 1 label).
+
+    This object parses CSV files (aka "seg2label" file) written in the following format:
+    
+            <start_time><separator><end_time><separator><label>
+            <start_time><separator><end_time><separator><label>
+            <start_time><separator><end_time><separator><label>
+            ...
+
+    where <start_time> and <end_time> are given in seconds (see test/data/*.seg
+    for some examples).
+
+    Every audio file must have a corresponding seg2label file in a
+    seg2label_files_root. seg2label_files_root must have the same structure as
+    the audio files root.
+
+    An label_file argument must be set to specify the set of labels to
+    be used. This file is a simple list of label, i.e.:
+
+            <label>
+            <label>
+            <label>
+            ...
+
+    All segments with a label which is not in the list specified in label_file
+    will have their label set to segment.CommonLabels.unknown.
+    """
 
     def __init__(self,
             seg2label_files_root,
@@ -63,6 +121,18 @@ class CSVSegmentLabelParser(SegmentLabelParser):
             audio_file_extension=".wav",
             seg_file_extension=".seg",
             seg_file_separator=","):
+        """Create a label list.
+
+        Args:
+            seg2label_files_root: root path of the seg2label files.
+            separator (optional): character used as a separator in the
+                seg2label files
+            label_file: file containing the list of labels to be
+                used.
+            audio_file_extension (optional)
+            seg_file_extension (optional)
+            seg_file_separator (optional)
+         """
 
         self._seg2label_files_root = seg2label_files_root
         self._label_file = label_file
@@ -78,6 +148,12 @@ class CSVSegmentLabelParser(SegmentLabelParser):
         self._label_list = sorted(list(self._label_list))
 
     def get_segment_container(self, audio_path):
+        """Returns a segment container with all the segments set to the labels
+        specified in the seg2label files
+        
+        Args:
+            audio_path
+        """
 
         seg_file_path_tuple = (self._seg2label_files_root, audio_path.replace(self._audio_file_extension, self._seg_file_extension))
 
@@ -88,4 +164,5 @@ class CSVSegmentLabelParser(SegmentLabelParser):
             seg_file_separator=self._seg_file_separator)
     
     def get_labels(self):
+        """Returns the list of labels"""
         return self._label_list
