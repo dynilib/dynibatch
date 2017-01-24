5e3aacdee8f132758e26ceba1d35acafba6916fd
ROGER Vincent <roger.vins.11@gmail.com>
2017-01-24 18:56:53 +0100

    Correct libdyni/__init__.py
    Now it can load a JSON config file and show a message if the config file is not found

--- libdyni/__init__.py
+++ libdyni/__init__.py
@@ -1,10 +1,13 @@
-import logging
+import os
+import logging.config
 import json
 
 # basic logging config
+LOGGING_FILENAME = os.path.join(os.path.dirname(__file__), '../logging.json')
 try:
-    with open('logging.json', 'rb') as f:
+    with open(LOGGING_FILENAME, 'r') as f:
         logging.config.dictConfig(json.load(f))
-except:
+except IOError:
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
                         datefmt="%Y-%m-%d %H:%M")
+    logging.debug('cannot open %s', LOGGING_FILENAME)
