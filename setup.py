c5da92a397d5648f7b8c0b1955de695229c604de
Julien Ricard <julien.ricard@gmail.com>
2017-03-31 17:44:53 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dynibatch',
+    version='0.9',
+    url='https://bitbucket.org/jul_dyni/dynibatch',
+    author='DYNI team',
+    author_email='dyni.contact@gmail.com',
+    license='MIT',
+    packages=['dynibatch', 'tests'],
+)
